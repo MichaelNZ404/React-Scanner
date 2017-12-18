191d549d89fc88bb4567246e15bd64db80294893
Michael Coleman <michael.coleman@eigentech.com>
2017-12-18 17:44:24 +0000

    should we use timer component?

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import './App.css';
 
 import Title from './components/Title';
 import Firewall from './components/Firewall';
+import Timer from './components/Timer';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // this.incrementCount = this.incrementCount.bind(this);
+    // this.sliderInput = this.sliderInput.bind(this);
+    // this.restart = this.restart.bind(this);
+    // this.colorFill = this.colorFill.bind(this);
+    this.state = {
+      title: 'Firewall Scanner',
+      firewall: {
+        0: 3,
+        1: 2,
+        4: 4,
+        6: 4,
+      }
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Title>Boobs</Title>
+        <Title>{this.state.title}</Title>
         <Firewall />
       </div>
     );
